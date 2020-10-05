ec95836405d6623bda6fbef5736041c3a6ad759e
Kirill Zemko <therealkirill@gmail.com>
2020-10-05 16:40:06 +0200

    Add timer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
     
+    // Tabs
+
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent = document.querySelectorAll('.tabcontent'),
           tabsParent = document.querySelector('.tabheader__items'); 
@@ -36,5 +38,64 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+    // Timer
+
+    const deadLine = '2020-12-31';
+
+    // функция возвращает остаток времени в виде объекта
+    function getTimeRemaining(endtime) {
+        // Date.parse разбирает строковое представление даты и возвращает кол-во милисекунд, прошедших с 01.01.1970
+        const t = Date.parse(endtime) - Date.parse(new Date), // разница между датами в милисекундах
+              days = Math.floor(t / (1000 * 60 * 60 * 24)),
+              hours = Math.floor( (t / (1000 * 60 * 60) % 24)), // делим на 24 и возвращаем остаточные часы от суток
+              minutes = Math.floor((t / (1000 * 60) % 60),
+              seconds = Math.floor((t / 1000) % 60));
+
+        // возвращаем объект
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    };
+
+    // функция добавляет 0 перед числом, если число меньше 10
+    function getZero (num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    };
+
+    // функция устанавливает таймер на страницу
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadLine);
 
 });
